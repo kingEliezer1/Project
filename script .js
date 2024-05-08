4a01af411df0c1a9c013da4ea5988d14bd18da02
kingEliezer1 <kevortepe@gmail.com>
2024-05-08 02:12:56 -0700

    this is my first commit

---
+++ script .js
@@ -0,0 +1,31 @@
+// var edem = ['MALE',32, 'King', 'beans']
+// document.write(edem.length);//
+
+
+
+
+
+// var fuits = ['Apple', 'Banana', "Mango", 'Orange', 'Papaya', 'Akple']
+// for (var i = 0; i < fruits.length; i++) {
+//       Iterates over array elemnts
+//      console.log(fruits[i] + '<br>'),
+//  }
+
+
+let shoes = ['Nike', 'Adidas', 'Puma', 'Cat']
+// let a = Adidas
+for (b = 0; b < shoes.length; b++)
+c = shoes[b] * a; {
+    document.write(shoes[b] + "<br>")
+}
+
+// let edem = ['jestice', 'solomon', 'Manfred', 'stephn', 'Frank', 'seni']
+// for (let e = 3; e < edem.length; e++) {
+//         document.write(edem[e] + "<br>");
+// }
+    
+
+// let key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+// for (let e = 0; e < key.length; e++) {
+//     document.write(key[e] + "<br>");
+// }
